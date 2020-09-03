a277363a5853f31856404662040de2cd3857f0ae
xeraz <66765257+xeraz@users.noreply.github.com>
2020-09-03 09:02:43 -0700

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -156,7 +156,7 @@ def start(update: Update, context: CallbackContext):
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup([[
                     InlineKeyboardButton(
-                        text="Add Saitama to your group",
+                        text="Add Uzaki to your group",
                         url="t.me/{}?startgroup=true".format(
                             context.bot.username))
                 ]]))
